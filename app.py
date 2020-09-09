43367cae7b008f22377ac7d3fc1c9571a07640cf
Chaim <chenming@dayuwuxian.com>
2020-09-09 18:01:50 +0800

    CollectorRegistry

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 # -*- coding:utf-8 -*-
 import prometheus_client
 from prometheus_client import Gauge, Counter
+from prometheus_client.core import CollectorRegistry, REGISTRY
 from flask import Response, Flask
 
 app = Flask(__name__)
-# 这次在 Gauge 类型里定义两个 labels，分别是"host"和"port",可以定义多个
-port_up = Gauge("Server_port", "monitor server port status.", ["host", "port"])
+# 实例化 REGISTRY
+REGISTRY = CollectorRegistry(auto_describe=False)
+# 这里又加了个 sertype 的 labels，定义的 labels 一定要被用到，否则会报错
+port_up = Gauge("Server_port", "monitor server port status.", ["sertype", "host", "port"], registry=REGISTRY)
 
 
 @app.route("/metrics")
 def requests_count():
-    # 传入不同的 lables 即可一次获得多项
-    port_up.labels("192.168.1.22", "2181").set(0)
-    port_up.labels("192.168.1.33", "2181").set(0)
-    port_up.labels("192.168.1.44", "2181").set(1)
+    # 模拟多个值传入
+    a = [{"sertype": "zookeeper", "host": "192.168.1.22", "port": "2181", "status": 0},
+         {"sertype": "zookeeper", "host": "192.168.1.33", "port": "2181", "status": 0},
+         {"sertype": "zookeeper", "host": "192.168.1.44", "port": "2181", "status": 1},
+         {"sertype": "mysql", "host": "192.168.1.88", "port": "3306", "status": 0},
+         {"sertype": "mysql", "host": "192.168.1.99", "port": "3306", "status": 1}]
+    for i in a:
+        ip = "".join(i.get("host"))
+        port = "".join(i.get("port"))
+        status = i.get("status")
+        sertype = "".join(i.get("sertype"))
+        port_up.labels(sertype, ip, port).set(status)
     return Response(prometheus_client.generate_latest(port_up),
                     mimetype="text/plain")
 
